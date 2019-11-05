e2749d5dc8040c3d0ed065178f0e94fcb16a868b
Artur Yelkin <Artur.Yelkin@itechart-group.com>
2019-11-05 21:13:56 +0300

    refactoring of routes

--- routes/user/create.js
+++ routes/user/create.js
@@ -1,25 +1,6 @@
 const { Router } = require("express");
-const { User } = require("../../database/models");
-const { validateNewUser } = require("../../utils/validation");
-const { ROLE_USER } = require("../../utils/constants");
-const { createPassword } = require("../../utils/password");
-
+const { registration } = require("../../controllers/auth");
 const router = Router();
 
-router.post("/", async (req, res) => {
-  const { firstName, surname, login, password } = req.body;
-  if (!validateNewUser(firstName, surname, login, password)) {
-    res.status(400).json({ message: "Wrong data to add new user!" });
-  } else {
-    await User.create({
-      firstName,
-      surname,
-      login,
-      password: await createPassword(password),
-      role: ROLE_USER
-    });
-    res.status(201).json({ message: "Successfully add new user!" });
-  }
-});
-
+router.post("/", registration);
 module.exports = router;
--- routes/user/login.js
+++ routes/user/login.js
@@ -1,46 +1,8 @@
 const { Router } = require("express");
-const jwt = require("jsonwebtoken");
-
-const { User } = require("../../database/models");
-const { validateLoginAndPassword } = require("../../utils/validation");
-const {
-  ROLE_USER,
-  SECRET,
-  EXPIRESIN_JWT
-} = require("../../utils/constants");
-const { resolvePassword } = require("../../utils/password");
+const { login } = require("../../controllers/auth");
 
 const router = Router();
 
-router.post("/", async (req, res) => {
-  const { login, password } = req.body;
-  if (!validateLoginAndPassword(login, password)) {
-    res
-      .status(400)
-      .json({ message: "Login or passowrd don't match required format" });
-  } else {
-    const user = await User.findOne({ where: { login } });
-    if (
-      !user ||
-      !(await resolvePassword(password, user.password)) ||
-      user.role !== ROLE_USER
-    ) {
-      res.status(400).json({ message: "Wrong username or password" });
-    } else {
-      const token = jwt.sign(
-        { login, password: user.password, role: ROLE_USER, id: user.id },
-        SECRET,
-        { expiresIn: EXPIRESIN_JWT }
-      );
-      res.status(200).json({
-        message: "Authentication is successful",
-        token,
-        firstName: user.firstName,
-        surname: user.surname,
-        id: user.id
-      });
-    }
-  }
-});
+router.post("/", login);
 
 module.exports = router;
