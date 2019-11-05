44f5659ff03c24a2984f3c5e30325484ad5714a8
Artur Yelkin <Artur.Yelkin@itechart-group.com>
2019-11-05 21:12:39 +0300

    add controllers

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,48 @@
-const jwt = require("jsonwebtoken");
 const { createPassword } = require("../utils/password");
+const { ROLE_USER } = require("../utils/constants");
+const {
+  validateLoginAndPassword,
+  validateNewUser
+} = require("../utils/validation");
 
 const authService = require("../services/auth");
 const userService = require("../services/user");
 
-const login = (req, res) => {
-  return authService.authenticate(req.body);//temporary 
+const login = async (req, res) => {
+  const { login, password } = req.body;
+  if (!validateLoginAndPassword(login, password)) {
+    res
+      .status(400)
+      .json({ message: "Login or passowrd don't match required format" });
+  }
+  const token = await authService.authenticate({ login, password });
+  if (!token) {
+    res.status(400).json({ message: "Wrong username or password" });
+  } else {
+    res.status(200).json({ message: "Successful auth", token });
+  }
+};
+
+const registration = async (req, res) => {
+  const { firstName, surname, login, password } = req.body;
+  if (!validateNewUser(firstName, surname, login, password)) {
+    res.status(400).json({ message: "Wrong data to add new user!" });
+  }
+  if (!(await userService.getUserByLogin(login))) {
+    userService.addUser({
+      firstName,
+      surname,
+      login,
+      password: await createPassword(password),
+      role: ROLE_USER
+    });
+    res.status(201).json({ message: "Successfully add new user!" });
+  } else {
+    res.status(400).json({ message: "Wrong data to add new user!" });
+  }
+};
+
+module.exports = {
+  login,
+  registration
 };
