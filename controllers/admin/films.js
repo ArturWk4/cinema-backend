4fd53cce182d02bbdfa768a435a0fa72d41be197
Artur Yelkin <Artur.Yelkin@itechart-group.com>
2019-11-21 17:14:07 +0300

    add dummy services

--- controllers/admin/films.js
+++ controllers/admin/films.js
@@ -3,5 +3,16 @@ const HttpStatus = require("http-status-codes");
 const filmsService = require("../../services/films");
 
 const addFilm = async (req, res) => {
-    const {title, description, startsAt, endsAt} = req.body;
+  const { title, description, startsAt, endsAt } = req.body;
+};
+
+const getFilm = async (req, res) => {};
+const getAllFilms = async (req, res) => {};
+const deleteFilm = async (req, res) => {};
+
+module.exports = {
+  addFilm,
+  getFilm,
+  getAllFilms,
+  deleteFilm
 };
