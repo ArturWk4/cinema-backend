2c434f850a1838aadc6d9d50c620f1eaf0013576
Artur Yelkin <Artur.Yelkin@itechart-group.com>
2019-11-22 22:14:01 +0300

    add films controller for admin

--- controllers/admin/films.js
+++ controllers/admin/films.js
@@ -1,14 +1,50 @@
 const HttpStatus = require("http-status-codes");
-
 const filmsService = require("../../services/films");
+const validateFilm = require("../../utils/validation");
 
 const addFilm = async (req, res) => {
-  const { title, description, startsAt, endsAt } = req.body;
+  const { title, description, duration, startsAt, endsAt } = req.body;
+  if (!validateFilm(title, description, duration, startsAt, endsAt)) {
+    res
+      .status(HttpStatus.BAD_REQUEST)
+      .json({ message: "Wrong data to add film" });
+  } else {
+    await filmsService.addFilm({
+      title,
+      description,
+      duration,
+      startsAt,
+      endsAt
+    });
+    res.status(HttpStatus.CREATED).end();
+  }
 };
 
-const getFilm = async (req, res) => {};
-const getAllFilms = async (req, res) => {};
-const deleteFilm = async (req, res) => {};
+const getFilm = id => async (req, res) => {
+  const film = await filmsService.getFilm(id);
+  if (!film) {
+    res.status(HttpStatus.NOT_FOUND).end();
+  } else {
+    res.status(HttpStatus.OK).json(film);
+  }
+};
+
+const getAllFilms = async (req, res) => {
+  const films = await filmsService.getAllFilms();
+  res.status(HttpStatus.OK).json(films);
+};
+
+const deleteFilm = id => async (req, res) => {
+  const film = filmsService.getFilm(id);
+  if (!film) {
+    res.status(HttpStatus.NOT_FOUND).end();
+  } else {
+    await deleteFilm(id);
+    res
+      .status(HttpStatus.OK)
+      .json({ message: `Successfully deleted film with id: ${id}` });
+  }
+};
 
 module.exports = {
   addFilm,
--- services/films.js
+++ services/films.js
@@ -11,4 +11,3 @@ module.exports = {
   getAllFilms,
   deleteFilm
 };
-
