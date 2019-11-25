6802988c87c2fbafc5cc8742f374a6d2c5d0146c
Artur Yelkin <Artur.Yelkin@itechart-group.com>
2019-11-25 21:57:04 +0300

    refactor services

--- services/films.js
+++ services/films.js
@@ -3,7 +3,7 @@ const Film = require("../database/models/film");
 const addFilm = async film => Film.create(film);
 const getFilm = async id => Film.findByPk(id);
 const getAllFilms = async () => Film.findAll();
-const deleteFilm = async id => getFilm(id).destroy();
+const deleteFilm = async id => Film.findByPk(id).destroy();
 
 module.exports = {
   addFilm,
