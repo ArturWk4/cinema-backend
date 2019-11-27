920876c7e74bec8712868e7957a5517cf318dadd
Artur Yelkin <Artur.Yelkin@itechart-group.com>
2019-11-27 15:40:36 +0300

    rename variable

--- services/auth.js
+++ services/auth.js
@@ -5,8 +5,8 @@ const { resolvePassword } = require("../utils/password");
 
 const authenticate = async ({ login, password, role }) => {
   const user = await User.findOne({ where: { login } });
-  const isResolvedPassword = await resolvePassword(password, user.password);
-  if (!user || !isResolvedPassword || user.role !== role) {
+  const validPassword = await resolvePassword(password, user.password);
+  if (!user || !validPassword || user.role !== role) {
     return null;
   } else {
     return (token = jwt.sign(
