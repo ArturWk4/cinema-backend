1f40cb0b25f02d5b9ec8150e5f81eca09d1906ce
Artur Yelkin <Artur.Yelkin@itechart-group.com>
2019-11-29 18:42:07 +0300

    add seatType logic

--- controllers/admin/seatTypes.js
+++ controllers/admin/seatTypes.js
@@ -1,22 +1,24 @@
 const HttpStatus = require("http-status-codes");
 const seatTypesServices = require("../../services/seatTypes");
 
-const addSeatType = (req, res) => {
-  const { type, cost } = req.body;
-  const seatType = await seatTypesServices.addSeatType({ type, cost });
+const addSeatType = async (req, res) => {
+  const { title, cost } = req.body;
+  const seatType = await seatTypesServices.addSeatType({ title, cost });
   if (!seatType) {
-    res.status(HttpStatus.BAD_REQUEST).json({ message: "Wrong data to add new seats type" });
+    res
+      .status(HttpStatus.BAD_REQUEST)
+      .json({ message: "Wrong data to add new seats type" });
   } else {
-    res.status(HttpStatus.CREATED);
+    res.status(HttpStatus.CREATED).end();
   }
 };
 
-const getAllSeatTypes = (req, res) => {
+const getAllSeatTypes = async (req, res) => {
   const seatTypes = await seatTypesServices.getAllSeatTypes();
   res.status(HttpStatus.OK).json(seatTypes);
-}
+};
 
-const getSeatType = (req, res) => {
+const getSeatType = async (req, res) => {
   const { id } = req.params;
   const seatType = await seatTypesServices.getSeatType(id);
   if (!seatType) {
@@ -24,10 +26,10 @@ const getSeatType = (req, res) => {
   } else {
     res.status(HttpStatus.OK).json(seatType);
   }
-}
+};
 
 module.exports = {
-    addSeatType,
-    getAllSeatTypes,
-    getSeatType
-}
+  addSeatType,
+  getAllSeatTypes,
+  getSeatType
+};
--- routes/admin/index.js
+++ routes/admin/index.js
@@ -2,10 +2,15 @@ const { Router } = require("express");
 const filmsRouter = require("./films");
 const cinemasRouter = require("./cinemas");
 const serviceRouter = require("./services");
+const seatsRouter = require("./seats");
+const seatTypesRouter = require("./seatTypes");
+
 const router = Router();
 
 router.use("/films", filmsRouter);
 router.use("/cinemas", cinemasRouter);
 router.use("/services", serviceRouter);
+router.use("/seats", seatsRouter);
+router.use("/seat-types", seatTypesRouter);
 
 module.exports = router;
