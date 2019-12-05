d8b877d1b1a260e6dfcaa48532984904c53513c3
Artur Yelkin <Artur.Yelkin@itechart-group.com>
2019-12-05 17:36:56 +0300

    refactor cinema serivces-layer

--- data-access/cinemas.js
+++ data-access/cinemas.js
@@ -3,9 +3,12 @@ const Cinema = require("../database/models/cinema");
 const addCinema = async cinema => Cinema.create(cinema);
 const getCinema = async id => Cinema.findByPk(id);
 const getAllCinemas = async () => Cinema.findAll();
+const getOrCreate = async (where, defaults) =>
+  Cinema.findOrCreate({ where, defaults });
 
 module.exports = {
   addCinema,
   getCinema,
-  getAllCinemas
+  getAllCinemas,
+  getOrCreate
 };
--- services/cinemas.js
+++ services/cinemas.js
@@ -2,21 +2,14 @@ const cityAccessor = require("../data-access/cities");
 const cinemaAccessor = require("../data-access/cinemas");
 
 const addCinema = async ({ title, city }) => {
-  const cities = await cityAccessor.getAllCities();
-  const cinemas = await cinemaAccessor.getAllCinemas();
-  let existingCity = cities.find(cityItem => cityItem.name == city);
-  let existingCinema = cinemas.find(cinemaItem => cinemaItem.title == title);
-
-  if (!existingCity) {
-    existingCity = await cityAccessor.addCity({ name: city });
-  } else {
-    if (existingCinema) {
-      return null;
-    }
-  }
-
-  const cinema = await cinemaAccessor.addCinema({ title });
-  cinema.setCity(existingCity);
+  const cityItem = await cityAccessor.getOrCreate(
+    { name: city },
+    { name: city }
+  );
+  const cinema = await cinemaAccessor.getOrCreate(
+    { title, cityId: cityItem[0].id },
+    { title, cityId: cityItem[0].id }
+  );
   return cinema;
 };
 
