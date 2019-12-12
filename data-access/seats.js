51fc6f9e0d9b9555074de133fa7d00fe82daf504
Artur Yelkin <Artur.Yelkin@itechart-group.com>
2019-12-12 19:45:30 +0300

    refactor seats logic

--- data-access/seats.js
+++ data-access/seats.js
@@ -3,12 +3,10 @@ const Seat = require("../database/models/seat");
 const addSeat = async seat => Seat.create(seat);
 const getAllSeats = async () => Seat.findAll();
 const getSeat = async id => Seat.findByPk(id);
-const getOrCreate = async (where, defaults) =>
-  Seat.findOrCreate({ where, defaults });
-
+const getSeatWhere = async where => Seat.findOne({ where });
 module.exports = {
   addSeat,
   getAllSeats,
   getSeat,
-  getOrCreate
+  getSeatWhere
 };
